89a247998d8ecb8712b72d61db45bffe9587105c
Chloe Chou <chloechou@chloes-mbp.attlocal.net>
2017-07-31 22:49:06 -0700

    add more functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,8 @@ function renderButtons(){
   for (var i = 0; i < disneyMovies.length; i++){
     var renderBtns = $('<button>');
     renderBtns.text(disneyMovies[i]);
-    renderBtns.addClass('btn btn-primary');
+    renderBtns.attr('data-name', disneyMovies[i]);
+    renderBtns.addClass('btn btn-primary movieBtn');
     $('#button-view').append(renderBtns);
   }
 }
@@ -21,10 +22,41 @@ $('#add-movie').click(function(){
   renderButtons();
 });
 
+function displayGifs(){
+  var movie = $(this).attr('data-name');
+  console.log(movie);
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC";
+
+  $.ajax({
+    url: queryURL, 
+    method: 'GET'
+  })
+    .done(function(response){
+      
+      var result = response.data;
+      console.log(result);
+
+      for (var i = 0; i < result.length; i++){
+        var gifDiv = $('<div class="item">');
+        var rating = result[i].rating;
+        var p = $('<p>').text('Rating: ' + rating);
+        var image = $('<img>');
+        image.attr('src', result[i].images.fixed_height.url);
+
+        gifDiv.prepend(p);
+        gifDiv.prepend(image);
+
+        $('#movie-view').prepend(gifDiv);
+      }
+    });
+}
 
 
 
 
 
+
+
+$(document).on("click", ".movieBtn", displayGifs);
 renderButtons();
 });
